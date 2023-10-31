bcb5e420c7d182e5b3d0372165d270842020788f
JuanDiego1423 <2202050@utrivieramaya.edu.mx>
2023-10-30 20:37:47 -0500

    first commit

---
+++ script/datatypes.js
@@ -0,0 +1,24 @@
+console.log(typeof 20)
+console.log(typeof 'HOLA MUNDO')
+console.log(typeof true)
+console.log(typeof 1.5) 
+console.log(typeof { nombre: "Juan", edad: 18 });
+console.log(typeof function suma(a, b) { return 15 + 30; });
+
+let varaibleSindato;
+let varaibleSindatos2;
+let varaibleSindatos3;
+let varaibleSindatos4;
+
+let variable = 1;
+let variable2 = 'Buenos Dias';
+let variable3 = 'hola mundo';
+let variable4 = 50;
+
+let nombre = 'Juan Diego',apellido = 'López Mis',estadoCivil = 'Soltero',pais = 'Mexico',edad = 18;
+
+
+let myAge = 18;
+let yourAge = 25;
+console.log("Mi Edad es: "+ myAge)
+console.log("Tu Edad es: "+ yourAge)
---
+++ script/variable.js
@@ -0,0 +1,10 @@
+//los comentarios pueden hacer que el código sea legible
+//Este es mi primer código JS.
+/*los comentarios pueden 
+hacer que el código sea legible, 
+fácil de reutilizar e informativo*/
+
+var mi_texto = 'Que tal'
+var Esfalso = false
+var Indefinido = undefined
+var nulo = null
